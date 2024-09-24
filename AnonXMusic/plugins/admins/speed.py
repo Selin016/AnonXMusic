9bc2c3d0f60be3b74c01d8a4fc92d3e3ddf95384
Selin016 <cselin622@gmail.com>
2024-09-24 16:18:23 +0300

    Update speed.py
    Signed-off-by: Selin016 <cselin622@gmail.com>

--- AnonXMusic/plugins/admins/speed.py
+++ AnonXMusic/plugins/admins/speed.py
@@ -14,7 +14,7 @@ checker = []
 
 
 @app.on_message(
-    filters.command(["cspeed", "speed", "cslow", "slow", "playback", "cplayback"])
+    filters.command(["cspeed", "speed", "atla", "cslow", "slow", "playback", "cplayback"])
     & filters.group
     & ~BANNED_USERS
 )
